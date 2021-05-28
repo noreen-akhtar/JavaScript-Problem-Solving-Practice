50f88d51029cc2ffd999df24a1ac17549c7bc098
noreen <noreenakhtar922@gmail.com>
2021-05-28 15:03:35 +0500

    Create a numeric, string, and boolean variable and print them.

---
+++ DefineVariables/app.js
@@ -0,0 +1,38 @@
+// Create a numeric, string, and boolean variable and print them.
+let int = 100;
+document.write("this is integer:", int);
+document.write("<br />");
+
+var float = 100.5;
+document.write("this is Float:", float);
+document.write("<br />");
+
+var hex = 0xfff;
+document.write("this is Hex:", hex);
+document.write("<br />");
+
+var exponential = 2.56e3;
+document.write("this is Exponential:", exponential);
+document.write("<br />");
+
+var octal = 030;
+document.write("this is octal:", octal);
+document.write("<br />");
+
+// string
+var myString1 = "Hello, ";
+var myString2 = "from string";
+document.write ( myString1 + myString2);
+// 
+var YES = true;
+var NO = false;
+
+if(YES)
+{
+    alert("This code block will be executed");
+}
+
+if(NO)
+{
+    alert("This code block will not be executed");
+}
