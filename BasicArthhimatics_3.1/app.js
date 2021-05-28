b06a1b00b73d037bfe9da7e88cba992471b6b62b
noreen <noreenakhtar922@gmail.com>
2021-05-28 15:38:28 +0500

    arthematic operations on varriable x and y

---
+++ BasicArthhimatics_3.1/app.js
@@ -0,0 +1,41 @@
+
+/*
+Example Addition, Subtraction, multiplication operators
+*/
+var x = 5 ;
+var y = 10 ;
+// var z = 15 ;
+ 
+//Addition
+p = x + y;
+document.write(" x + y is ", p);
+document.write("<br />");
+
+//Subtraction
+q = y - x;
+document.write(" x - y is ", q);
+document.write("<br />");
+
+//Multiplication
+r = x * y;
+document.write(" x * y is ", r);
+document.write("<br />");
+
+//Division
+s = y / x;
+document.write(" x / y is ", s);
+document.write("<br />");
+
+//Modulus
+t = x % 2;
+document.write(" x % 2 is ", t);
+document.write("<br />");
+
+//Increment
+x++;
+document.write(" x++ is ", x);
+document.write("<br />"); 
+
+//Decrement
+y--;
+document.write("y-- is ", y);
