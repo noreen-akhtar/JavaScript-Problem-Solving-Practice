06776f475c04ba5721429a9bfddd86701b117c5b
noreen <noreenakhtar922@gmail.com>
2021-06-01 22:40:30 +0500

    assign vales to variables

---
+++ Javascript-Coding-Task-1/checkIt/app.js
@@ -0,0 +1,35 @@
+/*04. Problem Name: checkIt
+
+Create variables and assign them different values as described below and check in the console its data type.
+ Write your results in the comments also.
+Assign an integer value to your variable.
+Assign a decimal value to your variable.
+Assign a boolean value to your variable.
+Assign any string to your variable. Remember to put quotation marks. Check its data type from the console. Now remove the quotation marks and see if it is still a string or do you see an error?
+Assign your variable a value of null. Check its type in the console.*/
+
+// checking type of integer
+let int = 10 ;
+console.log(int);
+// the result is 5
+
+// checking type of decimal
+let dec = 1.001;
+console.log(dec);
+// the result is 1.001
+
+// checking type of boolean 
+let bool = (100 > 99.9);
+console.log( bool);
+// the result is true
+
+// checking string value 
+let string = "96noor", check = 96;
+console.log(string);
+console.log(check);
+// answer is same for now because string is for words or combination of numbers or characters
+
+// checking empty variable 
+let x;
+console.log(x);
+// result is undefined because we not defined x
