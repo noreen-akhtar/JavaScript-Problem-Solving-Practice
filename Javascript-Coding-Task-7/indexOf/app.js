0ab64246d628599644612f11377ed88a773a911a
noreen <noreenakhtar922@gmail.com>
2021-06-07 22:53:38 +0500

    problem 2 index of task is done

---
+++ Javascript-Coding-Task-7/indexOf/app.js
@@ -0,0 +1,7 @@
+// 2) Problem Name: indexOf
+
+// Use indexOf function to attempt the same problem and print index of ‘Cricket’ member
+//Use indexOf function to attempt the same problem and print index of ‘Cricket’ member
+let games=['vollyball', 'cricket','football', 'badminton', 'hockey'];
+let b= games.indexOf('hockey',1);
+console.log(b);
