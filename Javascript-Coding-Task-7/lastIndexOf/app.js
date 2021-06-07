25c05a70cff760cdfaa3ed5e3cbe7bb76bca093b
noreen <noreenakhtar922@gmail.com>
2021-06-07 22:58:00 +0500

    problem 3 last index of task is done

---
+++ Javascript-Coding-Task-7/lastIndexOf/app.js
@@ -0,0 +1,7 @@
+
+// 3) Problem Name: lastIndexOf
+
+// Use the lastIndexOf function to find a ‘Cricket’ member. Start from the 3rd index and print its index.
+let games = ['vollyball', 'cricket','football', 'badminton', 'hockey'];
+let x = games.lastIndexOf('cricket',3);
+console.log(b);
