d4d1208f6a303fb15f63cff02d439b7d0e2ebd66
noreen <noreenakhtar922@gmail.com>
2021-06-14 17:22:51 +0500

    declare variable as 1 for continuous iteration

---
+++ Javascript-Coding-Task-4/InfiniteForLoop/app.js
@@ -0,0 +1,10 @@
+// 3. Problem Name: InfiniteForLoop
+
+// Create a for loop.
+
+// Use the given condition and see what happens. 
+// for (;;) {}
+
+for (i=1; i>=1; i++) {
+    console.log(i); // there is continueous iteration means infinite iteration 
+}
