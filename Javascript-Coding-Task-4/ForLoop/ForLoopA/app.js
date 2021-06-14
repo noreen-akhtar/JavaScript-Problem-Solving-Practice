10dc239d2964ac662fcf391a2cbe51ca0f406d0d
noreen <noreenakhtar922@gmail.com>
2021-06-14 17:14:52 +0500

    simple for loop in js as taking variable i

---
+++ Javascript-Coding-Task-4/ForLoop/ForLoopA/app.js
@@ -0,0 +1,9 @@
+// A. Create a for loop. 
+
+// Take a loop iterating variable as i.
+// The loop should iterate less than 50 times.
+// You have to print “Clap!” on the console.
+
+for(let i=1;i<50;i++){
+    console.log("Clap");
+}
