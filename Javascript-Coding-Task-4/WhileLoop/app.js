9b3f0d9f74e2dcc8ddd2bd870205dd1a30c13dbd
noreen <noreenakhtar922@gmail.com>
2021-06-14 17:24:56 +0500

    init var as a to print less than 10 times text

---
+++ Javascript-Coding-Task-4/WhileLoop/app.js
@@ -0,0 +1,11 @@
+// 4. Problem Name: WhileLoop
+
+// Take a loop control variable as a.
+// Initialize it.
+// Using While loop, print your name 10 times on the console.
+
+let a = 0;
+while (a < 10) {
+  a++; 
+  console.log('Noreen Akhtar');
+};
