0d09435ab599d86855cfc5d8fc113263699e749e
noreen <noreenakhtar922@gmail.com>
2021-06-14 17:20:16 +0500

    Use the decrement operator as -=3.

---
+++ Javascript-Coding-Task-4/ForLoopWithDecrementOperator/app.js
@@ -0,0 +1,12 @@
+// 2. Problem Name: ForLoopWithDecrementOperator
+
+// Create a for loop.
+
+// Take the loop iterating variable as i. 
+// Initialize it with 200
+// Print numbers from 200 till 0.
+// Use the decrement operator as -=3. 
+
+for(let i=200; i>0; i-=3){ // i -=3 >>>> 200-3 = 197
+    console.log(i);
+}
