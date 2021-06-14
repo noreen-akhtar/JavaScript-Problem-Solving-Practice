f24d6c4f30590e213ef1cec2bafa67c6750e3a3e
noreen <noreenakhtar922@gmail.com>
2021-06-14 17:18:59 +0500

    variable initialize to prit 0-199

---
+++ Javascript-Coding-Task-4/ForLoop/ForLoopB/app.js
@@ -0,0 +1,9 @@
+// B. Create a for loop.
+
+// Take the loop iterating variable as i.
+// The loop should start from 0 and end till 200.
+// Print numbers from 0-199.
+
+for(let i = 0; i < 200; i ++){
+    console.log(i);
+}
