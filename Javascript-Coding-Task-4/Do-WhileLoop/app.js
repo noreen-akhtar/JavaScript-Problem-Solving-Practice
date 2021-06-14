472c5c3c64a803577e49e17fd3c40981c5b38bfb
noreen <noreenakhtar922@gmail.com>
2021-06-14 17:17:02 +0500

    Take a loop control variable as a.

---
+++ Javascript-Coding-Task-4/Do-WhileLoop/app.js
@@ -0,0 +1,15 @@
+// 5. Problem Name: Do-WhileLoop
+
+// Take a loop control variable as a.
+// Initialize it.
+// Using While loop, print your name 10 times on the console in separate lines.
+
+let a = 1;
+const c = 10;
+let name = "Noreen";
+do {
+    a++; 
+    console.log(a+ ":" + name);
+    }
+    
+while (a <= c);  
